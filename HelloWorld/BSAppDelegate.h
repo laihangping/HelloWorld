110c93d29d6fda43c057fd3e45cf497882852c80
Hangping Lai <hangpinglai@hengtiansoft.com>
2013-11-18 15:36:34 +0800

    Initial Commit

---
+++ HelloWorld/BSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BSAppDelegate.h
+//  HelloWorld
+//
+//  Created by LaiHangping on 11/18/13.
+//  Copyright (c) 2013 Bangsun FIT Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
